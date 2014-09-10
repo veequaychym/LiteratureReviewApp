5841e3cf4a93eba6709922e907bbdf735217bc47
Victor <veequaychym@yahoo.com>
2014-09-10 20:11:06 -0300

    Compact layout tests

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,12 +1,3 @@
 module StaticPagesHelper
-  
-  # Returns the full title on a per-page basis.
-  def full_title(page_title)
-    base_title = "Literature Review App"
-    if page_title.empty?
-      base_title
-    else
-      "#{base_title} | #{page_title}"
-    end
-  end
+
 end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,22 +4,51 @@ describe "Static pages" do
   
   subject { page }
   
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1', text: heading) }
+    it { should have_title(full_title(page_title)) }
+  end
+  
   describe "Home page" do
     before { visit root_path }
-    it { should have_content('Literature Review App') }
-    it { should have_title(full_title('')) }
+    
+    let(:heading) { 'Literature Review App' }
+    let(:page_title) { '' }
+     
+    it_should_behave_like "all static pages"
     it { should_not have_title('| Home') }
   end
   
   describe "Help page" do
     before { visit help_path }
-    it { should have_content('Help') }
-    it { should have_title(full_title('Help')) }
+    
+    let(:heading) { 'Help' }
+    let(:page_title) { 'Help' }
+         
+    it_should_behave_like "all static pages"
   end
   
   describe "About app page" do
     before { visit about_path }
-    it { should have_content('About App') }
-    it { should have_title(full_title('About App')) }
+        
+    let(:heading) { 'About App' }
+    let(:page_title) { 'About App' }
+     
+    it_should_behave_like "all static pages"
+  end
+  
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    expect(page).to have_title(full_title('About App'))
+    click_link "Help"
+    expect(page).to have_title(full_title('Help'))
+    #click_link "Contact"
+    #expect(page).to # fill in
+    click_link "Home"
+    click_link "Sign up now!"
+    expect(page).to have_title(full_title('Sign up'))
+    #click_link "Literature Review App"
+    #expect(page).to have_title(full_title(''))
   end
 end
