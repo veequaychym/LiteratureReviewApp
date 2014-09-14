1f47bbb8ea4e3e893862955e21c2e747bfd5f31f
Victor <veequaychym@yahoo.com>
2014-09-14 11:25:06 -0300

    Finish sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,13 +10,14 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      #sign_in @user
+      sign_in @user
       flash[:success] = "Welcome to the Literature Review App!"
       redirect_to @user
     else
       render 'new'
     end
   end
+  
   
   private
 
